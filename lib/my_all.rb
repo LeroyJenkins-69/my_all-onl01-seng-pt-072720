462fcee35f30aeb6cec49b5a716eedf075dcf8f2
Kordell Parrish <parrishkordell@gmail.com>
2020-08-05 04:31:05 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,19 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  block_return_values = []
   while i < collection.length
-    yield(collection[i])
+    block_return_values << yield(collection[i])
     i = i + 1
     binding.pry 
   end
+
+  if block_return_values.include?(false)
+    false 
+  else
+    true
+  end
 end
+
+
+my_all([1,2,3]) {|i| i < 2}
